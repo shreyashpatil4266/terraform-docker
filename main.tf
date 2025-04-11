de2a38796ccf9678979fa6abe54e99d50bd1cfc2
Shreyash <patilsreyash422003@gmail.com>
2025-04-11 11:57:07 +0530

    Add Terraform configuration and .gitignore

---
+++ main.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 3.0.2"
+    }
+  }
+}
+
+provider "docker" {}
+
+resource "docker_image" "nginx" {
+  name = "nginx:latest"
+  keep_locally = false
+}
+
+resource "docker_container" "nginx" {
+  name  = "my-nginx-container"
+  image = docker_image.nginx.name
+
+  ports {
+    internal = 80
+    external = 8080
+  }
+}
